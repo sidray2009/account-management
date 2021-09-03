8f704f2cd4881a687c3da211af7ec9af07bf1fc4
Siddhartha Raychaudhuri <sira@su.dk>
2021-09-03 11:05:21 +0200

    set up project

---
+++ src/main/java/dk/db/manageaccounts/ManageaccountsApplication.java
@@ -0,0 +1,13 @@
+package dk.db.manageaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageaccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageaccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/db/manageaccounts/ManageaccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.db.manageaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageaccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
